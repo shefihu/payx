d13982efaf44359a3e593216a3cedef5eb487450
shefihu <rotimib0@gmail.com>
2022-07-17 16:18:04 +0100

    color dark

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -141,12 +141,12 @@ const Dashboard = () => {
         >
           <path
             fill-rule="evenodd"
-            className="text-green-400 dark:text-green-400"
+            className="fill-current text-green-400 dark:text-green-400"
             d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"
           />
           <path
             fill-rule="evenodd"
-            className="text-green-400 dark:text-green-400"
+            className="fill-current text-green-400 dark:text-green-400"
             d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
           />
         </svg>
@@ -165,11 +165,11 @@ const Dashboard = () => {
           viewBox="0 0 16 16"
         >
           <path
-            className="text-green-400 dark:text-green-400"
+            className="fill-current text-green-400 dark:text-green-400"
             d="M1.5 2A1.5 1.5 0 0 0 0 3.5v2h6a.5.5 0 0 1 .5.5c0 .253.08.644.306.958.207.288.557.542 1.194.542.637 0 .987-.254 1.194-.542.226-.314.306-.705.306-.958a.5.5 0 0 1 .5-.5h6v-2A1.5 1.5 0 0 0 14.5 2h-13z"
           />
           <path
-            className="text-green-400 dark:text-green-400"
+            className="fill-current text-green-400 dark:text-green-400"
             d="M16 6.5h-5.551a2.678 2.678 0 0 1-.443 1.042C9.613 8.088 8.963 8.5 8 8.5c-.963 0-1.613-.412-2.006-.958A2.679 2.679 0 0 1 5.551 6.5H0v6A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-6z"
           />
         </svg>
@@ -187,7 +187,7 @@ const Dashboard = () => {
           viewBox="0 0 16 16"
         >
           <path
-            className="text-green-400 dark:text-green-400"
+            className="fill-current text-green-400 dark:text-green-400"
             d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v1H0V4zm0 3v5a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7H0zm3 2h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1a1 1 0 0 1 1-1z"
           />
         </svg>
@@ -206,7 +206,7 @@ const Dashboard = () => {
         >
           <path
             fill-rule="evenodd"
-            className="text-green-400 dark:text-green-400"
+            className="fill-current text-green-400 dark:text-green-400"
             d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5zm14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5z"
           />
         </svg>
